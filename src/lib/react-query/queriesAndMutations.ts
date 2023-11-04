94ad9db0301a33473633f71c83a8e4df61c159ea
Prayag Narayan Parouha <prayagnp@gmail.com>
2023-11-05 02:35:44 +0530

    initial commit after creating signup functionality

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,16 @@
+import { INewUser } from '@/types';
+import { useQuery, useInfiniteQuery, useMutation, useQueryClient} from '@tanstack/react-query';
+import { createUserAccount, signInAccount } from '../appwrite/api';
+
+export const useCreateUserAccount = () => {
+    return useMutation({
+        mutationFn : (user: INewUser) => createUserAccount(user)
+    })
+}
+
+export const useSigninAccount = () => {
+    return useMutation({
+        mutationFn : (user: { email: string, password: string }) => signInAccount(user)
+    })
+}
+
